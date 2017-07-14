f03807e1df58af37d9566a23f3b34dc14f27975f
qdlyh <liang620523@163.com>
2017-07-14 19:38:36 +0800

    message

---
+++ vuepos/src/components/common/leftNav.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="left-nav">
+        <ul>
+            <li>
+                <i class="icon iconfont icon-jujia"></i>
+                <div>收银</div>
+            </li>
+    
+            <li>
+                <i class="icon iconfont icon-muyingwanju"></i>
+                <div>店铺</div>
+            </li>
+    
+            <li>
+                <i class="icon iconfont icon-shipin"></i>
+                <div>商品</div>
+            </li>
+    
+            <li>
+                <i class="icon iconfont icon-jiayongdianqi"></i>
+                <div>会员</div>
+            </li>
+    
+            <li>
+                <i class="icon iconfont icon-zhubaoshipin"></i>
+                <div>统计</div>
+            </li>
+            <li>
+                <i class="icon iconfont icon-jiajujiancai"></i>
+                <div>设置</div>
+            </li>
+        </ul>
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style>
+.left-nav {   
+    float: left;
+    width: 5%;
+    color: #fff;
+    font-size: 10px;
+    height: 100%;
+    background-color: #1D8ce0;
+}
+
+.iconfont {
+    font-size: 24px;
+}
+
+.left-nav ul {
+    padding: 0px;
+    margin: 0px;
+}
+
+.left-nav li {
+    list-style: none;
+    text-align: center;
+    border-bottom: 1px solid #20a0ff;
+    padding: 10px;
+}
+</style>
+
